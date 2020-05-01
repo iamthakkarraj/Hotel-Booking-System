2e6374445e17210c2a5aff7984498ed56e82a0a2
Raj thakkar <iamthakkarraj@gmail.com>
2020-05-02 01:57:17 +0530

    Added CORS in WebAPI

--- API/App_Start/WebApiConfig.cs
+++ API/App_Start/WebApiConfig.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace API {
 
     public static class WebApiConfig {
 
-        public static void Register(HttpConfiguration config) {            
-            
+        public static void Register(HttpConfiguration config) {
+
+            config.EnableCors(new EnableCorsAttribute("*", "*", "*"));
             config.MapHttpAttributeRoutes();
             config.Filters.Add(new BasicAuthenticationAttribute());
 
